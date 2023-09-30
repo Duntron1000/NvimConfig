54eba419b4a13bab6cd06b50afa61ae733877b04
Duntron1000 <90113909+Duntron1000@users.noreply.github.com>
2023-09-29 22:02:50 -0400

    initial commit of my nvim config

---
+++ lua/core/init.lua
@@ -0,0 +1,4 @@
+require("core.remap")
+require("core.lazy-config")
+require("core.laterlsp")
+require("core.set")
---
+++ lua/core/plugins/lsp.lua
@@ -0,0 +1,27 @@
+return{
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	-- LSP Support
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		lazy = true,
+		config = false,
+	},
+	{
+		'neovim/nvim-lspconfig',
+		dependencies = {
+			{'hrsh7th/cmp-nvim-lsp'},
+		}
+	},
+	-- Autocompletion
+	{
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+			{'L3MON4D3/LuaSnip'}
+		},
+	},
+	config = function()
+		print("this")
+	end,
+}
