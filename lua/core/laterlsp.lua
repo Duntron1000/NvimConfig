aa5301deaf8b009fbba38239e7cdf01d13d3f75a
Duntron1000 <90113909+Duntron1000@users.noreply.github.com>
2024-01-08 11:04:42 -0500

    Mon Jan 8

--- lua/core/laterlsp.lua
+++ lua/core/laterlsp.lua
@@ -26,7 +26,7 @@ function lspsetup()
 			['<C-Space>'] = cmp.mapping.complete(),
 
 			-- Navigate between snippet placeholder
-			['<C-f>'] = cmp_action.luasnip_jump_forward(),
+			['<Tab>'] = cmp_action.luasnip_jump_forward(),
 			['<C-b>'] = cmp_action.luasnip_jump_backward(),
 
 			-- Scroll up and down in the completion documentation
